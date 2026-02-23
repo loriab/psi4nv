9d22399a8bcb95a6a6086f93c5f693baa47fd4b2
Darren Hsu <dahsu@nvidia.com>
2026-02-23 18:11:12 -0500

    Cursor proposed code for using DSYEVD

--- psi4/src/psi4/libciomr/dsyev_ascending.cc
+++ psi4/src/psi4/libciomr/dsyev_ascending.cc
@@ -38,7 +38,8 @@
 
 namespace psi {
 /*!
-** DSYEV_ascending(): diagonalize a symmetric square matrix ('array') using LAPACK DSYEV
+** DSYEV_ascending(): diagonalize a symmetric square matrix ('array') using
+** LAPACK's divide-and-conquer DSYEVD for better performance on large matrices.
 **
 ** \param n      = number of rows (and columns)
 ** \param array  = matrix to diagonalize (2D row major array)
@@ -49,24 +50,28 @@ namespace psi {
 */
 [[nodiscard]] int DSYEV_ascending(const int N, const double* const* const array, double* e_vals,
                                   double* const* const e_vecs /* = nullptr*/) {
-    // We need to make a copy of the matrix before diagonalization, because LAPACK overwrites it.
-    // LAPACK also needs the mtx to be flattened to a 1D array, so a copy is inevitable.
-    // The new 1D array will correspond to a column-major array, suitable for LAPACK.
+    // Flatten row-major input to column-major 1D array for LAPACK.
     std::vector<double> tmp_matrix(N * N);
     for (int64_t i = 0, ij = 0; i < N; i++) {
         for (int64_t j = 0; j < N; j++, ij++) {
             tmp_matrix[ij] = array[j][i];
         }
     }
-    // LAPACK also needs some extra memory to store temporaries in
-    // TODO: query C_DSYEV for optimal workspace size
-    const int64_t workspace_size = 3 * N;
-    std::vector<double> tmp_work(workspace_size);
     const char jobtype = (e_vecs != nullptr) ? 'V' : 'N';
-    const int info = C_DSYEV(jobtype, 'U', N, tmp_matrix.data(), N, e_vals, tmp_work.data(), workspace_size);
+
+    // Query DSYEVD for optimal workspace sizes.
+    double work_query;
+    int iwork_query;
+    C_DSYEVD(jobtype, 'U', N, tmp_matrix.data(), N, e_vals, &work_query, -1, &iwork_query, -1);
+    const int lwork = static_cast<int>(work_query);
+    const int liwork = iwork_query;
+
+    std::vector<double> work(lwork);
+    std::vector<int> iwork(liwork);
+    const int info = C_DSYEVD(jobtype, 'U', N, tmp_matrix.data(), N, e_vals, work.data(), lwork, iwork.data(), liwork);
+
     if ((info == 0) && (e_vecs != nullptr)) {
-        // tmp_matrix has now been overwritten with the eigenvecs as the columns, flattened as column-major
-        // Copy them to the columns of a row-major 2D array
+        // Column-major eigenvectors back to row-major 2D array.
         for (int64_t j = 0, ij = 0; j < N; j++) {
             for (int64_t i = 0; i < N; i++, ij++) {
                 e_vecs[i][j] = tmp_matrix[ij];
